fd7966aaefdd0bb158d10582461bdd2fd4edb9e0
YassMaldini <yass.maldini@gmail.com>
2022-11-18 18:36:38 +0100

    chore: remains only built files

--- scripts/build.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-
-fs.removeSync('./build');
-fs.copySync('./src', './build');
-fs.copySync('./package.json', './build/package.json');
-fs.copySync('./README.md', './build/README.md');
-fs.copySync('./LICENSE', './build/LICENSE');
-fs.copySync('./index.d.ts', './build/index.d.ts');
--- scripts/setup-example.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs-extra');
-const { execSync } = require('child_process');
-
-fs.removeSync('./example/react-native-keyboard-accessory');
-fs.copySync('./src/.', './example/react-native-keyboard-accessory');
